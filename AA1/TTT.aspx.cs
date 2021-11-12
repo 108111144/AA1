f9de2e623ef0171d9c8bb99f622e9024a7d2d270
MIS803 <MIS803@30803_PC50>
2021-11-12 09:04:53 +0800

    加入專案檔案。

---
+++ AA1/TTT.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AA1
+{
+    public partial class TTT : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
